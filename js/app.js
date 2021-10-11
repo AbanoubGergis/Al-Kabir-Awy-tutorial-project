2b715584048ba00360677e8c022a4253c3f6918b
AbanoubGergis <abanoubgergis11@gmail.com>
2021-10-11 03:07:06 +0200

    Upload project files

---
+++ js/app.js
@@ -0,0 +1,33 @@
+/*
+* Manipulating the DOM exercise.
+* Exercise programmatically builds navigation,
+* scrolls to anchors from navigation,
+* and highlights section in viewport upon scrolling.
+*
+* Dependencies: None
+*
+* JS Version: ES2015/ES6
+*
+* JS Standard: ESlint
+
+*/
+/**
+* Define Global Variables
+*defining Sections and navigation list Global variables
+*/
+/**
+
+* End Global Variables
+* Start Helper Functions
+*
+*/
+/**
+* End Helper Functions
+* Begin Main Functions
+*
+
+
+
+
+*/
+// build the nav
